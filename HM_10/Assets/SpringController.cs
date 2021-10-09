96ba9bed3306f2639e2ddf951f09c2aea497536d
PastryChew <80216822+PastryChew@users.noreply.github.com>
2021-10-09 17:31:41 +0300

    11111

---
+++ HM_10/Assets/SpringController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpringController : MonoBehaviour
+{
+    private float springTimer;
+
+    public int springForce;
+    public Rigidbody rb;
+
+
+    void Start()
+    {
+        springTimer = -2;
+        rb = GetComponent<Rigidbody>();
+    }
+
+
+    void Update()
+    {
+        springTimer -= Time.deltaTime;
+
+        if (springTimer > -1)
+        {
+            rb.AddForce(1 * springForce, 0, 0, ForceMode.Impulse);
+            springTimer = -2;
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "Ball")
+        {
+            springTimer = 2;
+        }
+    }
+}
